00c0b77262e3c7e50dfc66ff0ea8460fab94bdad
akashcts <155516094+akashcts@users.noreply.github.com>
2024-01-03 12:26:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,10 @@ streamlit.text(' 🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 
+streamlit.title('My Mom New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣Omega 3 and Blueberry Oatmeal')
+streamlit.text(' 🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
+
